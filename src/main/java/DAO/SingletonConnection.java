875d0903b1348f7a6d7682cc5f97395046cc30e4
Mohamed Wala Chebil <40633583+yuno2l@users.noreply.github.com>
2023-10-02 17:37:44 +0100

    rev

--- src/main/java/DAO/SingletonConnection.java
+++ src/main/java/DAO/SingletonConnection.java
@@ -7,7 +7,7 @@ public class SingletonConnection {
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionproduit","root","enne");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionproduit","root","");
 			System.out.println("connection etablie");
 		}catch (ClassNotFoundException e) {
 			e.printStackTrace();
